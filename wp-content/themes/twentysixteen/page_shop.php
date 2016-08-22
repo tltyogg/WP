dad4950c946ab2a6fedb6c8369930d88b51d4994
Sara Carbaugh <sara@saracarbaugh.com>
2016-08-22 11:46:53 -0400

    switched over to posts instead of repeater

--- wp-content/themes/twentysixteen/page_shop.php
+++ wp-content/themes/twentysixteen/page_shop.php
@@ -4,37 +4,61 @@ Template Name: Shop
 */
 ?>
 <?php get_header(); ?>
+<div id="content-wrapper">
+	<div id="full-wrapper" class="shoppage">
+        <div style="clear: both;"></div>
+
+		<div id="shop-linkbar"><?php wp_nav_menu( array('theme_location' =>   'secondary' ) ); ?></div>
 
 		<div class="shop-body">
-			<?php if( have_rows('product') ): ?>
+			<?php
+			$paged = ( get_query_var('paged') ) ? get_query_var('paged') : 1;
+			$query = array( 'post_type' => 'shop', 'posts_per_page' => '40', 'paged'  => $paged );
+			$wp_query = new WP_Query( $query ); if ( $wp_query->have_posts() ) : ?>
+				<ul class="theproducts">
+				<?php
+				$image = get_field('image');
+				$name = get_field('name');
+				$price = get_field('price');
+				$link = get_field('link');
+				 while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
 
-		<ul class="theproducts">
 
-		<?php while( have_rows('product') ): the_row();
+					<li class="product"><a href="<?php echo $link; ?>" />
 
-			// vars
-			$image = get_sub_field('image');
-			$name = get_sub_field('name');
-			$price = get_sub_field('price');
-			$link = get_sub_field('link');
+							<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>" />
+							<div class="footer">
+								<div class="top"><?php echo $name; ?></div>
+								<div class="bottom"><?php echo $price; ?></div>
+							</div>
 
-			?>
+						</a>
+					</li>
+				<?php endwhile; ?>
+			</ul>
 
-			<li class="product"><a href="<?php echo $link; ?>" />
+			<div class="navigation">
+				<div class="load-more">
+					<div class="left"><?php previous_posts_link('Go Back') ?></div>
+					<div class="right"><?php next_posts_link('Keep Browsing') ?></div>
+				</div>
+			<?php endif; ?>
+		</div>
 
-					<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>" />
-			    <div class="footer">
-						<div class="top"><?php echo $name; ?></div>
-						<div class="bottom"><?php echo $price; ?></div>
-					</div>
+		<div class="shop-bottom">
+			<div class="left">
+				<h3><?php the_field('help1', '15'); ?></h3>
+				<h4><?php the_field('help2', '15'); ?></h4>
+				<a href="/contact/">Contact Us ></a>
+			</div>
+			<div class="right">
+				<div class="advert"><?php the_field('partners_image', '15'); ?></div>
+			</div>
+			<div style="clear: both;"></div>
+			<div style="clear: both;"></div>
+		</div>
 
-				</a>
-			</li>
-
-		<?php endwhile; ?>
-
-		</ul>
-
-	<?php endif; ?>
 	</div>
+</div>
+</div>
 <?php get_footer('full'); ?>
